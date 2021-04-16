d6eb99493811d03eb6e71db79393b1b0cc85d15b
Harish <u5842378@anu.edu.au>
2021-04-17 00:53:32 +1000

    adding icon expanding draft

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
 import 'style.dart';
+import 'icon_expand.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,24 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ImageBanner("assets/images/kiyomizu-dera.jpg"),
-              TextSection("summary", "something1"),
-              TextSection("summary", "something1"),
-              TextSection("summary", "something1"),
-            ]),
-      ),
-      theme: ThemeData(
-          appBarTheme:
-              AppBarTheme(textTheme: TextTheme(title: AppBarTextStyle)),
-          textTheme: TextTheme(title: TitleTextStyle, body1: Body1TextStyle)),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+            body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Stack(children: [
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Task name"),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [IconExpand()])
+                    ]),
+                Container(
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.arrow_right,
+                        color: Colors.black, size: 50.0))
+              ])
+            ])));
   }
 }
