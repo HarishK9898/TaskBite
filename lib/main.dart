bac459fd288a3d0f81d3a18fbb92cc74a07e564e
Harish <u5842378@anu.edu.au>
2021-04-22 22:49:33 +1000

    major changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
-import 'image_banner.dart';
-import 'text_section.dart';
-import 'style.dart';
-import 'icon_expand.dart';
+import 'package:task_app/task_list.dart';
+import 'header.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Home());
 
-class MyApp extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('Home'),
-            ),
-            body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Stack(children: [
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Task name"),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [IconExpand()])
-                    ]),
-                Container(
-                    alignment: Alignment.centerRight,
-                    child: Icon(Icons.arrow_right,
-                        color: Colors.black, size: 50.0))
-              ])
-            ])));
+            body: Column(children: [
+      Header("Home", true),
+      SingleChildScrollView(
+          child:
+              ConstrainedBox(constraints: BoxConstraints(), child: TaskList()))
+    ])));
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
 
 const LargeTextSize = 26.0;
-const MediumTextSize = 20.0;
+const MediumTextSize = 17.0;
 const BodyTextSize = 16.0;
 
-const String FontNameDefault = 'Montserrat';
+const String FontNameDefault = 'Less Sans';
 
-const AppBarTextStyle = TextStyle(
+const TaskTextStyle = TextStyle(
   fontFamily: FontNameDefault,
   fontWeight: FontWeight.w300,
   fontSize: MediumTextSize,
-  color: Colors.white,
-);
-
-const TitleTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w300,
-  fontSize: LargeTextSize,
-  color: Colors.black,
-);
-
-const Body1TextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w300,
-  fontSize: BodyTextSize,
   color: Colors.black,
 );
